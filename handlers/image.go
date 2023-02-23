30f32336905fb378ba3ff401e97acc8067a414a7
Aelpxy <aelpxy@protonmail.com>
2023-02-23 23:19:37 +0600

    feat: add memory caching for faster performance

--- handlers/image.go
+++ handlers/image.go
@@ -11,10 +11,29 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"sync"
 	"time"
 
 	"github.com/chai2010/webp"
 )
+
+type imageCache struct {
+	cache map[string][]byte
+	mu    sync.RWMutex
+}
+
+func (c *imageCache) Get(key string) ([]byte, bool) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	data, ok := c.cache[key]
+	return data, ok
+}
+
+func (c *imageCache) Set(key string, data []byte) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.cache[key] = data
+}
 
 func ServeWebPImage(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
@@ -22,6 +41,36 @@ func ServeWebPImage(w http.ResponseWriter, r *http.Request) {
 	queryPath := r.URL.Query().Get("path")
 	if queryPath == "" {
 		http.Error(w, "Missing 'path' query parameter", http.StatusBadRequest)
+		return
+	}
+
+	hash := sha256.Sum256([]byte(queryPath))
+	cacheKey := hex.EncodeToString(hash[:])
+	cachePath := filepath.Join(os.TempDir(), cacheKey+".webp")
+
+	cacheStatus := "MISS"
+	cache, ok := r.Context().Value("cache").(*imageCache)
+	if ok {
+		if data, ok := cache.Get(cacheKey); ok {
+			cacheStatus = "HIT"
+			w.Header().Set("Cache-Status", cacheStatus)
+			w.Header().Set("Response-Time", fmt.Sprint(time.Since(start).Milliseconds()))
+			w.Header().Set("Content-Type", "image/webp")
+			w.Write(data)
+			return
+		}
+	}
+
+	webpData, err := os.ReadFile(cachePath)
+	if err == nil {
+		cacheStatus = "HIT"
+		if cache != nil {
+			cache.Set(cacheKey, webpData)
+		}
+		w.Header().Set("Cache-Status", cacheStatus)
+		w.Header().Set("Response-Time", fmt.Sprint(time.Since(start).Milliseconds()))
+		w.Header().Set("Content-Type", "image/webp")
+		w.Write(webpData)
 		return
 	}
 
@@ -38,39 +87,25 @@ func ServeWebPImage(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	hash := sha256.Sum256(imageData)
-	cacheKey := hex.EncodeToString(hash[:])
-	cachePath := filepath.Join(os.TempDir(), cacheKey+".webp")
-
-	var cacheStatus string
-
-	if _, err := os.Stat(cachePath); os.IsNotExist(err) {
-		img, _, err := image.Decode(bytes.NewReader(imageData))
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		webpData, err := webp.EncodeRGBA(img, 80)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		err = os.WriteFile(cachePath, webpData, 0644)
-		if err != nil {
-			log.Printf("Failed to write cache file: %s", err.Error())
-		}
-
-		cacheStatus = "MISS"
-	} else {
-		cacheStatus = "HIT"
-	}
-
-	webpData, err := os.ReadFile(cachePath)
+	img, _, err := image.Decode(bytes.NewReader(imageData))
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
+	}
+
+	webpData, err = webp.EncodeRGBA(img, 80)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = os.WriteFile(cachePath, webpData, 0644)
+	if err != nil {
+		log.Printf("Failed to write cache file: %s", err.Error())
+	}
+
+	if cache != nil {
+		cache.Set(cacheKey, webpData)
 	}
 
 	w.Header().Set("Cache-Status", cacheStatus)
