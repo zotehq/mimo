4960e69c64641121a986dd6e33b0f9ae21528578
Aelpxy <aelpxy@protonmail.com>
2023-02-23 23:15:38 +0600

    fix: Cache-Status header was not behaving prior to this

--- handlers/image.go
+++ handlers/image.go
@@ -2,6 +2,8 @@ package handlers
 
 import (
 	"bytes"
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
 	"image"
 	"io"
@@ -9,36 +11,17 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
-	"strings"
 	"time"
 
 	"github.com/chai2010/webp"
 )
 
 func ServeWebPImage(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+
 	queryPath := r.URL.Query().Get("path")
 	if queryPath == "" {
 		http.Error(w, "Missing 'path' query parameter", http.StatusBadRequest)
-		return
-	}
-
-	start := time.Now()
-
-	cacheKey := strings.ReplaceAll(queryPath, "/", "_")
-	cachePath := filepath.Join(os.TempDir(), cacheKey+".webp")
-
-	if _, err := os.Stat(cachePath); err == nil {
-		webpData, err := os.ReadFile(cachePath)
-		if err != nil {
-			log.Printf("Failed to read cache file: %s", err.Error())
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		w.Header().Set("Cache-Status", "HIT")
-		w.Header().Set("Response-Time", fmt.Sprint(time.Since(start).Milliseconds()))
-		w.Header().Set("Content-Type", "image/webp")
-		w.Write(webpData)
 		return
 	}
 
@@ -49,35 +32,48 @@ func ServeWebPImage(w http.ResponseWriter, r *http.Request) {
 	}
 	defer resp.Body.Close()
 
-	if resp.StatusCode != http.StatusOK {
-		http.Error(w, "Failed to fetch remote image", resp.StatusCode)
-		return
-	}
-
 	imageData, err := io.ReadAll(resp.Body)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	img, _, err := image.Decode(bytes.NewReader(imageData))
+	hash := sha256.Sum256(imageData)
+	cacheKey := hex.EncodeToString(hash[:])
+	cachePath := filepath.Join(os.TempDir(), cacheKey+".webp")
+
+	var cacheStatus string
+
+	if _, err := os.Stat(cachePath); os.IsNotExist(err) {
+		img, _, err := image.Decode(bytes.NewReader(imageData))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		webpData, err := webp.EncodeRGBA(img, 80)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		err = os.WriteFile(cachePath, webpData, 0644)
+		if err != nil {
+			log.Printf("Failed to write cache file: %s", err.Error())
+		}
+
+		cacheStatus = "MISS"
+	} else {
+		cacheStatus = "HIT"
+	}
+
+	webpData, err := os.ReadFile(cachePath)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	webpData, err := webp.EncodeRGBA(img, 80)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	err = os.WriteFile(cachePath, webpData, 0644)
-	if err != nil {
-		log.Printf("Failed to write cache file: %s", err.Error())
-	}
-
-	w.Header().Set("Cache-Status", "MISS")
+	w.Header().Set("Cache-Status", cacheStatus)
 	w.Header().Set("Response-Time", fmt.Sprint(time.Since(start).Milliseconds()))
 	w.Header().Set("Content-Type", "image/webp")
 	w.Write(webpData)
